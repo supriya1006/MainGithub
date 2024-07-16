9803eac47f6e81455d45816ac264f45c5b569f28
Pranju884 <Pranjalipatil109@gmail.com>
2024-07-16 23:51:22 +0530

    abc

--- src/test/java/Tutorial4/Demo4.java
+++ src/test/java/Tutorial4/Demo4.java
@@ -3,8 +3,8 @@ package Tutorial4;
 public class Demo4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("code automate by Pranjali automation tester");
 	}
 
 }
